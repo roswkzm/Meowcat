bfbe297e4517937357c8c754712a01d66c57065b
이진영 <roswkzm@naver.com>
2022-02-27 18:15:18 +0900

    Viewpager2 에서 미리보기 생성, 넘길때 애니메이션 적용

--- app/src/main/java/com/example/meowcat/ContentDetailActivity.kt
+++ app/src/main/java/com/example/meowcat/ContentDetailActivity.kt
@@ -23,17 +23,18 @@ class ContentDetailActivity : AppCompatActivity() {
 
         val adapter = ViewpagerAdapter(destinationUid!!)
 
+        contentDetail_viewpager.offscreenPageLimit = 1      // 현재 페이지를 기준으로 얼마만큼 떨어진 page를 미리 생성할것인가
         contentDetail_viewpager.adapter = adapter
 
+        // 페이지 이동 애니메이션
+        var transform = CompositePageTransformer()
+        transform.addTransformer(MarginPageTransformer(8))
 
-//        var transform = CompositePageTransformer()
-//        transform.addTransformer(MarginPageTransformer(8))
-//
-//        transform.addTransformer(ViewPager2.PageTransformer{ view: View, fl: Float ->
-//            var v = 1-Math.abs(fl)
-//            view.scaleY = 0.8f + v * 0.2f
-//        })
-//
-//        contentDetail_viewpager.setPageTransformer(transform)
+        transform.addTransformer(ViewPager2.PageTransformer{ view: View, fl: Float ->
+            var v = 1-Math.abs(fl)
+            view.scaleY = 0.8f + v * 0.2f
+        })
+        contentDetail_viewpager.setPageTransformer(transform)
+
     }
 }
