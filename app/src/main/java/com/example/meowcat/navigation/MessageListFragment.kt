a017ddf7b2ca1891bd6c006e5010bd4d32647b1e
이진영 <roswkzm@naver.com>
2022-03-09 18:46:16 +0900

    Coroutine으로 비동기 함수들을 처리완료 MessageListFragment.kt 구현완료

--- app/src/main/java/com/example/meowcat/navigation/MessageListFragment.kt
+++ app/src/main/java/com/example/meowcat/navigation/MessageListFragment.kt
@@ -1,69 +1,100 @@
 package com.example.meowcat.navigation
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.example.meowcat.MessageActivity
 import com.example.meowcat.R
 import com.example.meowcat.navigation.model.ChatDTO
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
-import kotlinx.android.synthetic.main.activity_message_fragment.*
 import kotlinx.android.synthetic.main.activity_message_fragment.view.*
 import kotlinx.android.synthetic.main.item_chat_list.view.*
-import kotlinx.android.synthetic.main.item_message.view.*
+import kotlinx.coroutines.*
 import kotlinx.coroutines.tasks.await
-import kotlin.math.log
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MessageListFragment : Fragment(){
 
     val uid : String = FirebaseAuth.getInstance().currentUser!!.uid
-    var chatRoomUid : ArrayList<String> = arrayListOf()
+    var chatRoomUids : ArrayList<String> = arrayListOf()
     var chatDTOs : ArrayList<ChatDTO> = arrayListOf()
     var messageDTOs : ArrayList<ChatDTO.MessageDTO> = arrayListOf()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.activity_message_fragment, container, false)
 
-        getChatRoomList()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            var getChatRoomUid = async { setChatRoomUid() }.await()
+            var getLastMessage = async { setLastMessage(getChatRoomUid) }.await()
+
+
+            withContext(Dispatchers.Main){
+                Log.d("ㅎㅇㅎㅇ0", "어댑터 연결 전")
+                view.messageListFragment_recyclerView.adapter = MessageListRecyclerView(getLastMessage)
+                Log.d("ㅎㅇㅎㅇ0", "어댑터 연결 후")
+                view.messageListFragment_recyclerView.layoutManager = LinearLayoutManager(activity)
+                Log.d("ㅎㅇㅎㅇ0", "레이아웃 연결 후")
+
+            }
+        }
 
         return view
     }
 
-    fun getChatRoomList(){
-        FirebaseFirestore.getInstance().collection("chatRooms").whereArrayContains("users", uid).addSnapshotListener { value, error ->
-            if (value == null) return@addSnapshotListener
-            for (snapshot in value.documents){
-                chatRoomUid.add(snapshot.id)
-                Log.d("ㅎㅇㅎㅇchatRoomUid", chatRoomUid.toString())
-                chatDTOs.add(snapshot.toObject(ChatDTO::class.java)!!)
-                Log.d("ㅎㅇㅎㅇchatDTOs", chatDTOs.toString())
-            }
-        Log.d("ㅎㅇㅎㅇ", "어댑터 연결 전")
-        messageListFragment_recyclerView.adapter = MessageListRecyclerViewAdapter()
-        Log.d("ㅎㅇㅎㅇ", "어댑터 연결 후")
-        messageListFragment_recyclerView.layoutManager = LinearLayoutManager(activity)
-        Log.d("ㅎㅇㅎㅇ", "레이아웃 연결 후")
+    suspend fun setChatRoomUid() : ArrayList<String>{
+        chatRoomUids.clear()
+        chatDTOs.clear()
+        var documents = FirebaseFirestore.getInstance().collection("chatRooms").whereArrayContains("users",uid).get().await().documents
+        for (snapshot in documents){
+            Log.d("ㅎㅇㅎㅇ", snapshot.id)
+            chatRoomUids.add(snapshot.id)
+            Log.d("ㅎㅇㅎㅇ", snapshot.data!!["users"].toString())
+            chatDTOs.add(snapshot.toObject(ChatDTO::class.java)!!)
         }
-
+        Log.d("ㅎㅇㅎㅇ", "이름 삭제 전")
+        for (i in 0 until chatRoomUids.size){
+            chatDTOs[i].users.remove(uid)
+            Log.d("ㅎㅇㅎㅇ", chatDTOs[i].toString())
+        }
+        Log.d("ㅎㅇㅎㅇ", "이름 삭제 후")
+        Log.d("ㅎㅇㅎㅇchatDTOs 완결", chatDTOs.toString())
+        return chatRoomUids
     }
 
-    inner class MessageListRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    suspend fun setLastMessage(chatRoomUid : ArrayList<String>) : ArrayList<ChatDTO.MessageDTO>{
+        messageDTOs.clear()
+        for (i in 0 until chatRoomUid.size){
+            var documents = FirebaseFirestore.getInstance().collection("chatRooms").document(chatRoomUid[i]).collection("comments")
+                .orderBy("timestamp",Query.Direction.DESCENDING).limit(1).get().await().documents
+            for (snapshot in documents){
+                messageDTOs.add(snapshot.toObject(ChatDTO.MessageDTO::class.java)!!)
+                Log.d("ㅎㅇㅎㅇmessageDTOs 단건조회", messageDTOs.toString())
+            }
+        }
+        Log.d("ㅎㅇㅎㅇmessageDTOs 완료", messageDTOs.toString())
+        return messageDTOs
+    }
+
+    inner class MessageListRecyclerView(lastMessageDTO : ArrayList<ChatDTO.MessageDTO>) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+        var lastMessage : ArrayList<ChatDTO.MessageDTO> = arrayListOf()
 
         init {
-            for (i in 0 until chatRoomUid.size){
-                chatDTOs[i].users.remove(uid)       // 유저 정보에서 내정보를 지우니 상대방이 누군지 판단 가능하게 함
-                Log.d("ㅎㅇㅎㅇchatDTOs에서 uid 삭제후", chatDTOs[i].users.toString())
-
-            }
-
+            lastMessage = lastMessageDTO
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
@@ -86,16 +117,18 @@ class MessageListFragment : Fragment(){
                         viewHolder.item_chat_list_userNickName.text = value.result!!["userNickName"].toString()
                     }
                 }
+            // 마지막 메시지
+            viewHolder.item_chat_list_lastMessage.text = lastMessage[position].message
 
-//            // 마지막 메시지 출력
-//            viewHolder.item_chat_list_lastMessage.text = messageDTOs[position].message
-//
-//            // 마지막 시간 출력
-//            viewHolder.item_chat_list_timeStamp.text = messageDTOs[position].timestamp.toString()
+            val dateFormat = SimpleDateFormat("yyyy-MM-dd kk:mm", Locale("ko","KR"))
+            val sendTime = dateFormat.format(Date(lastMessage[position].timestamp!!))
+
+            viewHolder.item_chat_list_timeStamp.text = sendTime
+
         }
 
         override fun getItemCount(): Int {
-            return chatRoomUid.size
+            return chatRoomUids.size
         }
 
     }
